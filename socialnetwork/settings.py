3a928709f69df71cdce90a5ece4009be48b4d310
ymushet <18755772+ymushet@users.noreply.github.com>
2020-03-25 14:22:35 +0300

    [add] working dokerfile and migrations

--- socialnetwork/settings.py
+++ socialnetwork/settings.py
@@ -83,6 +83,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'postgres',
         'USER': 'postgres',
+        'PASSWORD': 'postgres',
         'HOST': 'db', # set in docker-compose.yml
         'PORT': 5432 # default postgres port
     }
